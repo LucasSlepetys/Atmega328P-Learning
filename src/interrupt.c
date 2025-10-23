659d9a1ea499f17c07ed2428a86b6af675f78916
Lucas <lucas.slepetyss@gmail.com>
2025-10-23 15:12:46 +0200

    Interrupt

---
+++ src/interrupt.c
@@ -0,0 +1,74 @@
+
+// #define F_CPU 16000000UL
+
+// #include <stdint.h>
+// #include <avr/io.h>
+// #include <util/delay.h>
+
+// #include <avr/interrupt.h>
+
+// volatile int isOn = 0;
+// volatile uint32_t last_press_time = 0;
+
+
+// //Interrupt sub-routine:
+// //when button in PD2 is pressed
+// ISR(INT0_vect) {
+  
+
+//     //TURN On LED
+//     if(isOn){
+//       PORTD &= ~(1 << PD6);
+//       isOn = 0;
+//     } else{
+//       PORTD |= (1 << PD6);
+//       isOn = 1;
+//     }
+
+//     //debounce
+//     _delay_ms(100); 
+// }
+
+// //ISR(INT2_vect) {}
+
+
+
+// int main(void) {
+
+//   //Interrupts: PD2 and PD3
+//   //Make them inputs
+//   DDRD &= 0b11110011;
+
+//   //Outputs:
+//   DDRD |= 0b11110000;
+
+//   //Enable pull up resistor for PD2 and PD3
+//   PORTD = 0b00001100;
+
+//   //Set both interrups as falling edge trigger (button is pressed only):
+//   EICRA = (1 << ISC11) | (1 << ISC01); //the rest default to zero
+
+//   //Enable both external interrups:
+//   EIMSK = (1 << INT1) | (1 << INT0);   //the rest default to zero
+
+
+//   //Enable global interrupt so all interrups work:
+//   sei();
+
+//   while(1) 
+//   {
+
+//     //blink LED in PD4:
+//     PORTD |= (1 << PD4);
+//     _delay_ms(2000);
+//     PORTD &= ~(1 << PD4);
+//     _delay_ms(2000);
+
+
+
+//   }
+
+//   return 0;
+// }
+// //needs to add millis with internal clock
+
